c4fc85de9af586f0c44413bfc8658f7f8a61b06f
arttttt <artem-bambalov@yandex.ru>
2019-01-10 22:52:43 +0300

    extend hidl
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- hidl.mk
+++ hidl.mk
@@ -1,3 +1,14 @@
+# Audio
+PRODUCT_PACKAGES += \
+	android.hardware.audio@2.0-impl \
+	android.hardware.audio@2.0-service \
+	android.hardware.audio.effect@2.0-impl
+
+# DRM HAL
+PRODUCT_PACKAGES += \
+	android.hardware.drm@1.0-service \
+	android.hardware.drm@1.0-impl
+
 # Graphics
 PRODUCT_PACKAGES += \
 	android.hardware.graphics.composer@2.1-service \
@@ -5,3 +16,8 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.allocator@2.0-impl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+	android.hardware.keymaster@3.0-impl \
+	android.hardware.keymaster@3.0-service
