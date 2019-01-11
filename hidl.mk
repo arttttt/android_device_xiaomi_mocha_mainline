7424b714bf195134a6c9713c940bb5e51703f361
arttttt <artem-bambalov@yandex.ru>
2019-01-11 03:18:18 +0300

    WIP
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- hidl.mk
+++ hidl.mk
@@ -21,3 +21,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.keymaster@3.0-impl \
 	android.hardware.keymaster@3.0-service
+
+# Renderscript
+PRODUCT_PACKAGES += \
+	android.hardware.renderscript@1.0-impl \
+	android.hardware.renderscript@1.0-service
